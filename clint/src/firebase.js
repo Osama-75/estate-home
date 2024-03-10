0b97677b6408c243894bb75d681fc4a4aee49bec
OsamaNasser <ooooosama446@gmail.com>
2024-03-10 17:18:22 +0200

    Home

---
+++ clint/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8EIcMZ-1vUS7bOjehrNN_3i9yNZXnZjQ",
+  authDomain: "ver-mern.firebaseapp.com",
+  projectId: "ver-mern",
+  storageBucket: "ver-mern.appspot.com",
+  messagingSenderId: "1008938586396",
+  appId: "1:1008938586396:web:8250f83a97e554c65d6ad9",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
